595b8d022ffa9f5f6676ccf86184f6d519752be5
Ally <alw399@pitt.edu>
2025-01-13 17:30:43 -0500

    refit linear terms with interactions

--- src/interaction.py
+++ src/interaction.py
@@ -5,7 +5,7 @@ import os
 from sklearn.linear_model import LinearRegression
 from knockpy import KnockoffFilter
 
-from .utils import get_sigLFs, load_z_matrix
+from util import get_sigLFs
 
 class Interaction():
 
@@ -21,17 +21,22 @@ class Interaction():
         self.l = self.plm_embedding.shape[1]
 
         self.interaction_terms = self.get_interaction_terms(self.z_matrix, self.plm_embedding)
-    
+
+        self.beta_interaction = None
+        self.rejections = None
+
     def get_z_matrix(self):
         z_matrix = pd.read_csv(os.path.join(self.slide_outs, 'z_matrix.csv'), index_col=0)
         z_matrix = z_matrix[self.sig_LFs]
         return z_matrix.values
 
     def get_plm_embedding(self):
-        pass
-
-    def get_y():
-        pass
+        print('Warning: hardcoded path to plm embedding')
+        return np.load('/ix/djishnu/Jane/SLIDE_PLM/jing_expansion/KIR+CD8_testVAE_hidden_layer2.npy')
+        
+    def get_y(self):
+        print('Warning: hardcoded path to y')
+        return pd.read_csv('/ix/djishnu/Jane/SLIDESWING/jing_data/KIR+CD8/data/KIR+CD8_Yexpanded_filtered85.csv')['Y'].values
 
     @staticmethod
     def get_interaction_terms(z_matrix, plm_embedding):
@@ -61,35 +66,26 @@ class Interaction():
 
     def compute(self):
 
-        # maybe fix this set up later
-
         z_matrix = self.z_matrix
         plm_embedding = self.plm_embedding
         interaction_terms = self.interaction_terms
         y = self.y.copy()
         n, k, l = self.n, self.k, self.l
 
-        # Fit z-matrix in linear part to get LP
-
-        LP, beta = self.fit_linear(z_matrix, y)
+        # fit Z1_sig and Z2_plm interaction terms to y
+        
+        interaction_terms = interaction_terms.reshape(n,k*l)
+        all_terms = np.concatenate([z_matrix, interaction_terms], axis=1)
+        _, beta_all = self.fit_linear(all_terms, y)
 
         # Identify significant interaction terms
 
-        rejections = self.filter_knockoffs(interaction_terms.reshape(n, k*l), y)
-        interaction_terms = interaction_terms * rejections 
+        rejections = self.filter_knockoffs(all_terms.reshape(n, -1), y)
+        self.rejections = rejections
 
-        # subtract LP from y
-
-        y = y - LP
-
-        # fit interaction terms to y
-
-        _, beta_interaction = self.fit_linear(interaction_terms, y)
+        beta_interaction = beta_all[k:].copy()
         beta_interaction = beta_interaction.reshape(k,l)
 
-        # mask out non-significant interaction terms again (in case)
-
-        beta_interaction = beta_interaction * rejections
         return beta_interaction
 
 
--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
-import os 
 from glob import glob
 import pandas as pd
+from pathlib import Path
+
 
 def get_sigLFs(slide_outs):
     '''
@@ -8,8 +9,7 @@ def get_sigLFs(slide_outs):
     @param slide_outs: path to the best performining slide outputs
     @return sigLFs: list of significant latent factors
     '''
-
-    sig_LFs = glob(os.path.join(slide_outs, '/*gene_list*'))
+    sig_LFs = glob(str(Path(slide_outs) / '*_list*'))
     sig_LFs = [f"Z{path.replace('.txt','').rsplit('Z', 1)[1]}" for path in sig_LFs]
 
     return sig_LFs
